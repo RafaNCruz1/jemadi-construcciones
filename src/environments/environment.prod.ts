09a15d9ccb3bbedbcd75986fcc34abf9e59e016e
Jesus Nava <jrnc9802@gmail.com>
2020-10-25 01:26:21 -0600

    Agrega variable de entorno mapToken

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapBoxToken: 'pk.eyJ1IjoicmFmYWVsbmM5OCIsImEiOiJja2duNXBqcXAwMTMwMnhvaXo2N3hpYmUwIn0.997SBiX7PZtC6_-C2hxwFg'
 };
